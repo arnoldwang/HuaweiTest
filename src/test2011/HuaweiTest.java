d2641db819a7b3e6cee0a01971a863b6562de47a
arnoldwang <arnold1990@sina.com>
2014-09-11 15:52:02 +0800

    add other questions

--- src/test2011/HuaweiTest.java
+++ src/test2011/HuaweiTest.java
@@ -39,32 +39,38 @@ public class HuaweiTest {
 	 */
 	public static void getOutString(String str, int len, int m) {
 		String[] s = str.split(",");
-		List<String> list = new ArrayList<String>(Arrays.asList(s)); 
+		List<String> list = new ArrayList<String>(Arrays.asList(s));
 		int i = 0;
 		int count = 1;
-		while(len > 0){
-			while(count != m){
+		while (len > 0) {
+			while (count != m) {
 				count++;
-				if(i < len-1)
+				if (i < len - 1)
 					i++;
 				else
 					i = 0;
-					
+
 			}
 			m = Integer.parseInt(list.get(i));
 			System.out.println(list.remove(i));
-			i = i == (len - 1) ? 0 : i;//注意这里！
+			i = i == (len - 1) ? 0 : i;// 注意这里！
 			len--;
 			count = 1;
 		}
 	}
 
-	public static int getMyRet(String str){  
+	/**
+	 * 输入一个表达式，没有括号，数字小于0-9之间，输出计算结果，所有的中间结果化为整形。
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static int getMyRet(String str) {
 		Stack<Integer> numStack = new Stack<Integer>();
 		Stack<Character> charStack = new Stack<Character>();
 		char[] c = str.toCharArray();
-		for(int i = 0; i < c.length; i++){
-			switch(c[i]){
+		for (int i = 0; i < c.length; i++) {
+			switch (c[i]) {
 			case '0':
 			case '1':
 			case '2':
@@ -74,7 +80,7 @@ public class HuaweiTest {
 			case '6':
 			case '7':
 			case '8':
-			case '9': 
+			case '9':
 				numStack.push(Character.digit(c[i], 10));
 				break;
 			case '+':
@@ -83,24 +89,175 @@ public class HuaweiTest {
 				break;
 			case '*':
 				i++;
-				numStack.push((int)(numStack.pop()*Character.digit(c[i], 10)));
+				numStack.push((int) (numStack.pop() * Character.digit(c[i], 10)));
 				break;
 			case '/':
 				i++;
-				numStack.push((int)(numStack.pop()/Character.digit(c[i], 10)));
+				numStack.push((int) (numStack.pop() / Character.digit(c[i], 10)));
 				break;
 			}
 		}
-		while(!charStack.isEmpty()){
+		while (!charStack.isEmpty()) {
 			int num2 = numStack.pop();
 			int num1 = numStack.pop();
-			if(charStack.pop() == '+')
+			if (charStack.pop() == '+')
 				numStack.push(num1 + num2);
 			else
 				numStack.push(num1 - num2);
 		}
 		return numStack.pop();
 	}
+
+	/**
+	 * 选秀节目打分，分为专家评委和大众评委，score[] 数组里面存储每个评委打的分数，judge_type[] 里存储与 score[]
+	 * 数组对应的评委类别， judge_type[i] == 1，表示专家评委，judge_type[i] == 2，表示大众评委，n表示评委总数。
+	 * 打分规则如下：专家评委和大众评委的分数先分别取一个平均分（平均分取整），然后，总分 = 专家评委平均分 * 0.6 + 大众评委
+	 * *0.4，总分取整。 如果没有大众评委，则 总分 = 专家评委平均分，总分取整。函数最终返回选手得分。
+	 * 
+	 * @param score
+	 * @param judge_type
+	 * @param n
+	 * @return
+	 */
+	public static int cal_score(int score[], int judge_type[], int n) {
+		int result = 0;
+		int perScore = 0, norScore = 0;
+		int perNum = 0, norNum = 0;
+		for (int i = 0; i < n; i++) {
+			if (judge_type[i] == 1) {
+				perScore += score[i];
+				perNum++;
+			}
+			if (judge_type[i] == 2) {
+				norScore += score[i];
+				norNum++;
+			}
+		}
+		result = norNum == 0 ? (int) (perScore / perNum) : (int) (perScore
+				/ perNum * 0.6)
+				+ (int) (norScore / norNum * 0.4);
+		return result;
+	}
+
+	/**
+	 * 给定一个数组input[] ，如果数组长度n为奇数，则将数组中最大的元素放到 output[] 数组最中间的位置，
+	 * 如果数组长度n为偶数，则将数组中最大的元素放到 output[] 数组中间两个位置偏右的那个位置上，
+	 * 然后再按从大到小的顺序，依次在第一个位置的两边，按照一左一右的顺序，依次存放剩下的数。
+	 * 
+	 * @param input
+	 * @param n
+	 * @param output
+	 */
+	public static void sort(int input[], int n, int output[]) {
+		quickSort(input, 0, input.length - 1);
+		int p = n / 2;
+		int q = n / 2 - 1;
+		for (int i = 0; i < n; i++) {
+			if (i % 2 == 0) {
+				output[p] = input[i];
+				p++;
+			} else {
+				output[q] = input[i];
+				q--;
+			}
+		}
+		for (int temp : output)
+			System.out.print(temp + " ");
+	}
+
+	private static void quickSort(int[] input, int left, int right) {
+		if (left + 1 < right) {
+			int pivot = median3(input, left, right);
+
+			int i = left + 1, j = right;
+			while (true) {
+				while (input[++i] > pivot) {
+				}
+				while (input[--j] < pivot) {
+				}
+
+				if (i < j)
+					swap(input, i, j);
+				else
+					break;
+			}
+			swap(input, j, left + 1);
+			quickSort(input, left, j - 1);
+			quickSort(input, j + 1, right);
+		} else {
+			if (input[left] < input[right])
+				swap(input, left, right);
+		}
+
+	}
+
+	private static int median3(int[] input, int left, int right) {
+		// TODO Auto-generated method stub
+		int center = (left + right) / 2;
+		if (input[left] < input[center])
+			swap(input, left, center);
+		if (input[center] < input[right])
+			swap(input, center, right);
+		if (input[left] < input[center])
+			swap(input, left, center);
+		swap(input, center, left + 1);
+		return input[left + 1];
+	}
+
+	private static void swap(int[] input, int a, int b) {
+		// TODO Auto-generated method stub
+		int temp = input[a];
+		input[a] = input[b];
+		input[b] = temp;
+	}
+
+	/**
+	 * 操作系统任务调度问题。操作系统任务分为系统任务和用户任务两种。其中，系统任务的优先级 < 50，用户任务的优先级 >= 50且 <= 255。
+	 * 优先级大于255的为非法任务，应予以剔除。现有一任务队列task[]，长度为n，task中的元素值表示任务的优先级，数值越小，优先级越高。
+	 * 函数scheduler实现如下功能，将task[] 中的任务按照系统任务、用户任务依次存放到
+	 * system_task[]数组和user_task[]数组中（数组中元素的值是任务在task[]数组中的下标），
+	 * 并且优先级高的任务排在前面，优先级相同的任务按照入队顺序排列（即先入队的任务排在前面），数组元素为-1表示结束。
+	 * 
+	 * @param task
+	 * @param n
+	 * @param system_task
+	 * @param user_task
+	 */
+	public static void scheduler(int task[], int n, int system_task[],
+			int user_task[]) {
+		int p = 0, q = 0;
+		for (int i = 0; i < n; i++) {
+			if (task[i] < 50) {
+				int m = p;
+				while (m > 0) {
+					if (task[system_task[m - 1]] < task[i])
+						break;
+					system_task[m] = system_task[m - 1];
+					m--;
+				}
+				system_task[m] = i;
+				p++;
+			}
+			if (task[i] >= 50 && task[i] <= 255) {
+				int m = q;
+				while (m > 0) {
+					if (task[user_task[m - 1]] < task[i])
+						break;
+					user_task[m] = user_task[m - 1];
+					m--;
+				}
+				user_task[m] = i;
+				q++;
+			}
+		}
+		system_task[p] = -1;
+		user_task[q] = -1;
+		for (int temp : system_task)
+			System.out.print(temp + " ");
+		for (int temp : user_task)
+			System.out.print(temp + " ");
+	}
+
 	
 	public static void main(String[] args) {
 		String s1 = "1,3,5";
@@ -109,7 +266,17 @@ public class HuaweiTest {
 		int len1 = 3;
 		int len2 = 5;
 		int len3 = 4;
-		System.out.println(HuaweiTest.getMyRet("3+8*2/9-2"));
+		// System.out.println(HuaweiTest.getMyRet("3+8*2/9-2"));
+		int score[] = { 34, 53, 65, 75, 64 };
+		int judge_type[] = { 1, 1, 1, 2, 2 };
+		// System.out.print(HuaweiTest.cal_score(score, judge_type, 5));
+		int input[] = { 3, 6, 1, 9, 7, 8 };
+		int output[] = new int[6];
+		// HuaweiTest.sort(input, 6, output);
+		int task[] = { 0, 30, 155, 1, 80, 300, 170, 40, 99 };
+		int system_task[] = new int[9];
+		int user_task[] = new int[9];
+		HuaweiTest.scheduler(task, 9, system_task, user_task);
 	}
 
 }
